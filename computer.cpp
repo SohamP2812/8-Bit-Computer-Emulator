dcc38f886bd51b24cfae3549e53dadc1921d9842
Soham Parmar <s24parma@uwaterloo.ca>
2022-06-23 11:48:22 -0400

    add user commands

--- computer.cpp
+++ computer.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <iomanip>
 
 using namespace std;
 
@@ -14,8 +18,8 @@ struct Computer {
 
     unsigned char PC = 0x00;
 
-    unsigned char A;
-    unsigned char B;
+    unsigned char A = 0x00;
+    unsigned char B = 0x00;
 };
 
 unsigned char peek(struct Computer *state, unsigned short addr) {
@@ -91,9 +95,67 @@ int simulate(struct Computer *state) {
     return 0;
 }
 
+void print_hex(unsigned char value) {
+    cout << "0x" << setw(2) << setfill('0') << hex << (int) static_cast<unsigned>(value) << endl;
+}
+
 int main() {
     struct Computer state;
+
     unsigned char data[7] = {0x52, 0x27, 0x31, 0xF0, 0, 0, 0};
+
+    while (true) {
+        cout << "> ";
+        
+        string input{};
+
+        std::getline(cin, input);
+
+        std::istringstream iss{ input };
+
+        std::vector<std::string> args{};
+
+        std::string str2{};
+        
+        while (iss >> str2) {
+            args.push_back(str2);
+        }
+
+        if(args[0] == "GET") {
+            if(args[1] == "A") {
+                print_hex(state.A);
+            } else if (args[1] == "B") {
+                print_hex(state.B);
+            } else if (args[1] == "PC") {
+                print_hex(state.PC);
+            }
+        } else if (args[0] == "SET") {
+            if(args[1] == "A") {
+                state.A = static_cast<unsigned>(stoi(args[2], 0, 16));
+            } else if (args[1] == "B") {
+                state.B = static_cast<unsigned>(stoi(args[2], 0, 16));
+            } else if (args[1] == "PC") {
+                state.PC = static_cast<unsigned>(stoi(args[2], 0 ,16));
+            }
+        } else if (args[0] == "PEEK") { 
+           print_hex(peek(&state, (short)stoi(args[1])));
+        } else if (args[0] == "POKE") { 
+            poke(&state, (short)stoi(args[1]), static_cast<unsigned>(stoi(args[2], 0, 16)));
+        } else if (args[0] == "DUMP") {
+            cout << "REGISTERS:" << endl << endl;
+            cout << "A - ";
+            print_hex(state.A);
+            cout << "B - ";
+            print_hex(state.B);
+            cout << "PC - ";
+            print_hex(state.PC);
+        } else if (args[0] == "STEP") {
+            step(&state);
+        } else if (args[0] == "LOAD") {
+            mem_copy(&state, (short)stoi(args[1]), data, 7);
+        }
+    }
+
     mem_copy(&state, 0, data, 7);
     // mem_set(&state, 0, 0x00, 6);
     // poke(&state, 0, 0x49);
